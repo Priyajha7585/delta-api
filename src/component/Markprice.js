a37d801823352a8e29454afae1e14f221d72b157
Priya_Jha <priyajha7585@gmail.com>
2022-08-20 19:43:35 +0530

    Updated project

---
+++ src/component/Markprice.js
@@ -0,0 +1,53 @@
+import React from 'react'
+
+function Markprice({symbol, underlying_asset_symbol}) {
+
+    const [markPrice, setMarkPrice] = React.useState("");
+
+    const ws = new WebSocket("wss://production-esocket.delta.exchange");
+
+    const apiCall = {
+        "type": "subscribe",
+        "payload": {
+            "channels": [
+                {
+                    "name": "v2/ticker",
+                    "symbols": [
+                        symbol, underlying_asset_symbol
+                    ]
+                }
+            ]
+        }
+      };
+
+    ws.onopen = (event) => {
+        ws.send(JSON.stringify(apiCall));
+    };
+
+    ws.onmessage = function (event) {
+        console.log(event)
+        const json = JSON.parse(event.data);
+        // console.log(json['mark_price'])
+        console.log(json)
+        try {
+          if (typeof json['mark_price']!=="undefined") {
+            console.log(json['mark_price'])
+            setMarkPrice(json['mark_price']);
+          }
+        } catch (err) {
+          console.log(err);
+        }
+      };
+    // ws.onclose = (event) => {
+    //   console.log("connection closed")
+    // }
+    // ws.close(
+    //   console.log("connection closed")
+    // );
+
+  return (
+    <div>{markPrice}</div>
+  )
+}
+
+export default Markprice
---
+++ src/component/Table.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import axios from 'axios';
+import Markprice from './Markprice';
+import './table.css'
+
+function Table() {
+    const [products, setProducts] = React.useState([]);
+    React.useEffect(()=>{
+        axios.get("https://api.delta.exchange/v2/products")
+        .then((res)=>{
+            setProducts(res.data.result);
+            // console.log(products);
+            // console.log(res.data.result)
+        })
+        .catch((err)=>console.log(err))
+    }, [])
+  return (
+    <div>
+        <table class="table-fixed">
+            <thead>
+            <tr>
+                <th>Symbol</th>
+                <th>Description</th>
+                <th>Underlying Asset</th>
+                <th>Mark Price</th>
+            </tr>
+            </thead>
+            <tbody>
+            {products.map((product)=>(
+            <tr>
+            <td key={product.id}>{product.symbol}</td>
+            <td key={product.id}>{product.description}</td>
+            <td key={product.id}>{product.underlying_asset.symbol}</td>
+            <td key={product.id}><Markprice symbol={product.symbol} underlying_asset_symbol={product.underlying_asset.symbol}/></td>
+            </tr>
+            
+        ))}</tbody>
+        </table>
+        
+    </div>
+  )
+}
+
+export default Table
